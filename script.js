0458fc05d177b3d502edd06846213a29852013e7
deadworldinc <98756973+deadworldinc@users.noreply.github.com>
2025-07-09 14:46:35 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+window.onload = function() {
+    let streamPlaying = false;
+    let buttonPlayImage = document.getElementById("buttonPlayImage");
+    let mainAudio = document.getElementById("mainAudio");
+    let spanTrackName = document.getElementById("spanTrackName");
+    let buttonLike = document.getElementById("buttonLike");
+
+    buttonPlay.onclick = function () {
+        mainAudio.play();
+        if (streamPlaying) {
+            buttonPlayImage.classList.add("fa-play");
+            streamPlaying = false;
+            mainAudio.volume = 0;
+        } else {
+            buttonPlayImage.classList.remove("fa-play");
+            buttonPlayImage.classList.add("fa-pause");
+            streamPlaying = true;
+            mainAudio.volume = 1;
+        }
+    }
+    
+    setInterval(() => {
+        fetch("https://golemworld.ru:8443/status-json.xsl")
+            .then(jsonResponse => jsonResponse.json())
+            .then(json => {
+                spanTrackName.innerHTML = json.icestats.source.title;
+                mainAudio.title = `Golem Radio: ${json.icestats.source.title}`;
+            });
+    }, 1000);
+}
